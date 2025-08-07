db1cdb851ed25b951edaed5f8db6e7a8e7381f37
Ward-R <4577045+Ward-R@users.noreply.github.com>
2025-08-07 13:59:18 +0900

    parsing the json file. have basic struct made. Data not formatted or clean

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+const JMAQuakeURL = "https://www.jma.go.jp/bosai/quake/data/list.json"
+
+type QuakeSummary struct {
+	// can add more from the JMA JSON, just putting a couple to get it working for now.
+	ID         string `json:"eid"`
+	EnLocation string `json:"en_anm"`
+	Magnitude  string `json:"mag"`
+}
 
 func main() {
 	fmt.Println("Hello, Jishin API!")
+
+	res, err := http.Get(JMAQuakeURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if res.StatusCode > 400 {
+		log.Fatalf("Response failed with status code: %d and \nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Unmarshall body into events slice of QuakeSummary structs
+
+	var events []QuakeSummary
+	err = json.Unmarshal([]byte(body), &events)
+
+	fmt.Printf("%+v", events)
 }
